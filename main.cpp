56e009649dc63a466c6665844ce211db604412e5
Moukla <153756019+Moukla@users.noreply.github.com>
2023-12-17 15:30:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=100;i++){
+        if(i % 3==0){
+            cout<<"FIZZ"<<endl;
+        }else if(i % 5==0){
+            cout<<"BUZZ"<<endl;
+        }else{cout<<i<<endl;}
+    }
+    return 0;
+}
